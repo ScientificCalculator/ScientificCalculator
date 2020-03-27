d5683654a6ab046238ab61841fe67e82331c0296
mohamed hammad <mohamedhammad7779@gmail.com>
2020-03-27 17:58:13 +0200

    Add files via upload

---
+++ function.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int convertDecimalToBinary(int n);
+int convertDecimalToOctal(int n);
+void decToHexa(int n); 
+int convertBinaryToDecimal(int n);
+int convertOctalToDecimal(int n);
+int convertBinaryToOctal( int bin);
+int convertOctalToBinary(int oct);
+int convertBinaryTohexa(int bin);
+int convertOctalTohexa(int octal);
+void HexaToDecimal(char hex[32] );
+void HexaToOctal(char hex[17]);
+void HexaToBinary(char hex[17]);
